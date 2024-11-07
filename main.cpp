4a2a80d9774da24c1ec3a3cdbd0ca6a53aa0b05d
Alessandra Avhril Paner <alessandraavhrilpaner041404@gmail.com>
2024-11-07 16:15:43 +0800

    Added empty switch statement to main.cpp
    ...

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int main()
     while(true){
         cout << "Sample code here" << endl;
     }
+
+    switch(){
+        case 1:
+            cout << "" ;
+            break;
+        default:
+            cout << "" ;
+    }
     
 //sample
 
